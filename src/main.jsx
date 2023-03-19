87bcfd454bcbd4c91f224e207ef92f949b47dd4c
Seanghay Yath <seanghay.dev@gmail.com>
2023-03-19 11:22:02 +0700

    remove deps

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import MyWorker from './worker.js?worker'
 
-import wasm_heif from '@saschazar/wasm-heif';
-
-console.log(wasm_heif({
-	onRuntimeInitialized() {
-		
-	}
-}))
-
 const worker = new MyWorker();
 
 ReactDOM.createRoot(document.getElementById("app")).render(
