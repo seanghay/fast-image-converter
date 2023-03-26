264c5cd7cf1dc809e130979b6892954bc1092b02
Seanghay Yath <seanghay.dev@gmail.com>
2023-03-26 13:19:05 +0700

    chore: msg

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,11 +4,12 @@ export default function Footer() {
 	return (
 		<>
 			<footer className="footer">
+				<p className="msg">All photos are converted locally in your browser!</p>
 				<p className="text-center color-secondary">
 					Supported formats <br />
-					.svg, .heif, .heic, .avif, .webp, .jpg, .png, .pdf VectorDrawable(.xml)
+					.svg, .heif, .heic, .avif, .webp, .jpg, .png, .pdf
+					VectorDrawable(.xml)
 				</p>
-
 				<p className="text-center text-sm color-secondary">
 					Made with ❤️ by{" "}
 					<a className="color-secondary" href="https://github.com/seanghay">
